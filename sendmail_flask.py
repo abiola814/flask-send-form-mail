085b4f738cf849c699527596b94de216c4feaa1a
oladunjoye abiola moses <oladunjoye1998@gmail.com>
2023-08-29 08:23:58 +0100

    Update sendmail_flask.py

--- sendmail_flask.py
+++ sendmail_flask.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Configure email settings for Gmail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
